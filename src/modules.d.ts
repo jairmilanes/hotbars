42a238a45cb3ceaf78876d92e72a9f17e8a70767
Jair Milanes <jmilanes.dev@gmail.com>
2022-12-20 16:54:03 -0500

    fix: missing module typo

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -3,6 +3,7 @@ declare module "handlebars-layouts";
 declare module "handlebars-utils";
 declare module "day-log-savings";
 declare module "lodash-id";
+declare module "@jmilanes/passport-remember-me"
 
 /*
 interface "@handlebars/parser" {
